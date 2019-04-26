628e347eebcec5b96d497dc13400682b8ad7c93c
Conrad Mearns <conradmearns@gmail.com>
2019-04-25 19:27:46 -0800

    Croppy

--- Software/project_starting_point/main.c
+++ Software/project_starting_point/main.c
@@ -37,7 +37,7 @@
  *  updates REG_CMD_READ after commands have been executed
  
  */
-        
+
 int main()
 {
     initialize_system();
@@ -46,37 +46,131 @@ int main()
     
 //    APP_Calibrate();
     
-    uint8_t x = 7;
     char buf[50];
+    struct system_variables sv;
     
-    int i;
-    struct system_variables sys_vars;
+    float temp_c_moving = 1;
+    float temp_f_moving = 1;
+    float humidi_moving = 1;
+    float moving_N = 200;
+    
+    int first = 0;
+    
+    // Sizes:   30  22  18  16
     
     while (1)
     {
+        system_variables_update(&sv);
+        
+        if (0 == first)
+        {
+            first++;
+            temp_c_moving = sv.temp_c;
+            temp_f_moving = sv.temp_f;
+            humidi_moving = sv.humidity;
+        }
+        
+        temp_c_moving -= temp_c_moving / moving_N;
+        temp_c_moving += sv.temp_c / moving_N;
+        
+        temp_f_moving -= temp_f_moving / moving_N;
+        temp_f_moving += sv.temp_f / moving_N;
+        
+        humidi_moving -= humidi_moving / moving_N;
+        humidi_moving += sv.humidity / moving_N;
+    
         API_LIB_BeginCoProList();
         API_CMD_DLSTART();
         
-        API_CLEAR_COLOR_RGB(0, 0, 0);
+//        API_CLEAR_COLOR_RGB(0, 0, 0);
+        API_CLEAR_COLOR_RGB(0xFF, 0xFF, 0xFF);
         API_CLEAR(1,1,1);
         
+//        API_COLOR_RGB(0xFF, 0xFF, 0xFF);
+        API_COLOR_RGB(0, 0, 0);
+
+        
+        sprintf(buf, "Date: %02d - %02d - %d  |  Time: %d:%02d", sv.day, sv.month, sv.year, sv.hour, sv.minute);
+        API_CMD_TEXT(10, 10, 22, 0, buf);
+        
+        sprintf(buf, "Latitude: %s  |  Longitude: %s", sv.latitude, sv.longitude);
+        API_CMD_TEXT(400, 10, 22, 0, buf);
+        
+        sprintf(buf, "Board Temperature (C): %0.3f (%f)", temp_c_moving, sv.temp_c);
+        API_CMD_TEXT(10, 40, 22, 0, buf);
+        
+        sprintf(buf, "Board Temperature (F): %0.3f (%f)", temp_f_moving, sv.temp_f);
+        API_CMD_TEXT(10, 70, 22, 0, buf);
+        
+        sprintf(buf, "Humidity: %.3f (%f)", humidi_moving, sv.humidity);
+        API_CMD_TEXT(10, 100, 22, 0, buf);
+        
+        sprintf(buf, "Tilt: %f", sv.tilt);
+        API_CMD_TEXT(10, 130, 22, 0, buf);
+        
+        sprintf(buf, "ADC_RAW: %d  |  ADC: %f", sv.adc_raw, sv.adc_nominal);
+        API_CMD_TEXT(10, 160, 22, 0, buf);
+        
+        
+        int lx=700,ly=130,ls=16;
+        
+        API_BEGIN(LINES);
+        API_COLOR_RGB(0, 0, 0);
+        API_LINE_WIDTH(4*16);
+        //Stalk
+        API_VERTEX2F((lx * 16), (ly * 16));
+        API_VERTEX2F((lx * 16), ((ly+220) * 16));
+        //Arrow
+        API_VERTEX2F((lx * 16), (ly * 16));
+        API_VERTEX2F(((lx-30) * 16), ((ly-20) * 16));
+        API_VERTEX2F((lx * 16), (ly * 16));
+        API_VERTEX2F(((lx+30) * 16), ((ly-20) * 16));
+        API_VERTEX2F((lx * 16), ((ly-70) * 16));
+        API_VERTEX2F(((lx-30) * 16), ((ly-20) * 16));
+        API_VERTEX2F((lx * 16), ((ly-70) * 16));
+        API_VERTEX2F(((lx+30) * 16), ((ly-20) * 16));
+        int yoff = 60;
+        int xoff = 60;
+        int yi, xi;
+        for (xi = -1; xi < 2; xi+=2)
+        {
+            for (yi = 0; yi < 3; yi++)
+            {
+                API_VERTEX2F((lx * 16), (((ly+18) + yoff*yi) * 16));
+                API_VERTEX2F(((lx - xoff*xi) * 16), (((ly-20) + yoff*yi) * 16));
+
+                API_VERTEX2F((lx * 16), (((ly+58) + yoff*yi) * 16));
+                API_VERTEX2F(((lx - xoff*xi) * 16), (((ly+20) + yoff*yi) * 16));
+
+                API_VERTEX2F(((lx - xoff*xi) * 16), (((ly-20) + yoff*yi) * 16));
+                API_VERTEX2F(((lx - xoff*xi) * 16), (((ly+20) + yoff*yi) * 16));
+            }
+        }
+        API_VERTEX2F((lx * 16), ((ly+180) * 16));
+        API_VERTEX2F(((lx-50) * 16), ((ly+200) * 16));
+        API_VERTEX2F(((lx-50) * 16), ((ly+200) * 16));
+        API_VERTEX2F(((lx-50) * 16), ((ly+250) * 16));
+
+        API_VERTEX2F((lx * 16), ((ly+190) * 16));
+        API_VERTEX2F(((lx+50) * 16), ((ly+210) * 16));
+        API_VERTEX2F(((lx+50) * 16), ((ly+210) * 16));
+        API_VERTEX2F(((lx+50) * 16), ((ly+270) * 16));
+        API_END();
+
+        API_BEGIN(FTPOINTS);                                       	
+        API_POINT_SIZE(20*16);
+        API_COLOR_RGB(0, 0, 0);                     
+        API_VERTEX2F((lx*16), ((ly+220)*16));
+        API_VERTEX2F(((lx-50)*16), ((ly+250)*16));
+        API_VERTEX2F(((lx+50)*16), ((ly+270)*16));
+        API_POINT_SIZE(12*16);
         API_COLOR_RGB(0xFF, 0xFF, 0xFF);
+        API_VERTEX2F((lx*16), ((ly+220)*16));
+        API_VERTEX2F(((lx-50)*16), ((ly+250)*16));
+        API_VERTEX2F(((lx+50)*16), ((ly+270)*16));
+        API_END();
         
-        x *= 43;
-        sprintf(buf, "Here's some nonsense %d", x);
-        API_CMD_TEXT(10, 10, 30, 0, buf);
         
-        x *= 23;
-        sprintf(buf, "Here's some more nonsense %d", x);
-        API_CMD_TEXT(10, 50, 22, 0, buf);
-        
-        x *= 723;
-        sprintf(buf, "[%d] is in brackets", x);
-        API_CMD_TEXT(10, 80, 18, 0, buf);
-        
-        x *= 901;
-        sprintf(buf, "heres sum %d", x);
-        API_CMD_TEXT(10, 110, 16, 0, buf);
         
         API_DISPLAY();
         API_CMD_SWAP();
@@ -84,16 +178,8 @@ int main()
         API_LIB_EndCoProList();
         API_LIB_AwaitCoProEmpty();
             
-        MCU_Delay_ms(250);
-        
-        i = 0;
-        while(i<1000)
-        {
-            i++;
-        }
-        system_variables_update(&sys_vars);
+//        MCU_Delay_ms(20);
     }
-
 }
 void initialize_system()
 {
