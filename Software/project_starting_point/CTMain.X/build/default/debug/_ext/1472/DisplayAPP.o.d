8a6c37d1c2fcfc6db09dd2a52cf27899aff7d68f
ryand323 <ryand323@gmail.com>
2019-04-25 18:50:34 -0700

    Compiles and Runs

---
+++ Software/project_starting_point/CTMain.X/build/default/debug/_ext/1472/DisplayAPP.o.d
@@ -0,0 +1,4 @@
+build/default/debug/_ext/1472/DisplayAPP.o:  \
+../DisplayAPP.c  \
+ ../DisplayLib.h  \
+../FT8xx.h 
---
+++ Software/project_starting_point/CTMain.X/build/default/debug/_ext/1472/EVE_Layer.o.d
@@ -0,0 +1,4 @@
+build/default/debug/_ext/1472/EVE_Layer.o:  \
+../EVE_Layer.c  \
+../Library.h  \
+ ../FT8xx.h 
